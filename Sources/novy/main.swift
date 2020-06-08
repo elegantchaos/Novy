1562f00c1b330c5d280b6e4477f2b65748fa3574
Sam Deane <sam@elegantchaos.com>
2020-06-08 12:53:23 +0100

    small beginnings

--- Sources/novy/main.swift
+++ Sources/novy/main.swift
@@ -1,1 +1,14 @@
-print("Hello, world!")
+import Foundation
+
+
+class Duplicator {
+    func clone(template: URL, into destination: URL) {
+        print("cloning from \(template) into \(destination).")
+    }
+}
+
+let duplicator = Duplicator()
+let template = URL(fileURLWithPath: "/Users/sam/Projects/novy/Extras/Templates/Example")
+let destination = URL(fileURLWithPath: "/Users/sam/Desktop")
+
+duplicator.clone(template: template, into: destination)
