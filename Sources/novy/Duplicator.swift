6cae5ee6ab8f2b934d885188b3aa4a1e6dbffebf
Sam Deane <sam@elegantchaos.com>
2020-06-09 11:54:51 +0100

    added import command

--- Sources/novy/Duplicator.swift
+++ Sources/novy/Duplicator.swift
@@ -9,24 +9,39 @@ import Files
 class Duplicator {
     let fileManager = FileManager.default
     
-    func clone(template: Folder, into destination: Folder) {
+    func `import`(project: Folder, into templates: Folder) {
+        let originalName = project.name.name
+        project.copy(to: templates, replacing: true)
+        let destination = templates.folder(name: project.name)
+        destination.forEach(order: .foldersFirst) { item in
+            let name = item.name.renamed(as: item.name.name.replacingOccurrences(of: originalName, with: "«project»"))
+            if name != item.name {
+                print("Renamed \(item.name) as \(name).")
+                item.rename(as: name)
+            }
+        }
+        
+        destination.forEach(filter: .files) { item in
+            if let file = item as? File, let text = file.asText {
+                let processed = text.replacingOccurrences(of: originalName, with: "«project»")
+                if processed != text {
+                    print("Substituted \(item.name)")
+                    file.write(as: processed)
+                }
+            }
+        }
+    }
+
+    func clone(template: Folder, into destination: Folder, substitutions: [String:String]) {
         print("cloning from \(template) into \(destination).")
         
         template.copy(to: destination)
-//        template.forEach(inParallelWith: destination, filter: .visible) { item, parallel in
-//            if item.isFile {
-//                let name = item.name.renamed(as: item.name.name.replacingOccurrences(of: "ActionStatus", with: "«project»"))
-//                item.copy(to: parallel!, as: name)
-//            }
-//        }
-        
-        template.forEach(order: .foldersFirst, recursive: true) { item in
-            if item.name.name.contains("ActionStatus") {
-                print("blah")
-            }
-            let name = item.name.renamed(as: item.name.name.replacingOccurrences(of: "ActionStatus", with: "«project»"))
-            if name != item.name {
-                item.rename(as: name)
+        template.forEach(order: .foldersFirst) { item in
+            for (key, value) in substitutions {
+                let name = item.name.renamed(as: item.name.name.replacingOccurrences(of: "«\(key)»", with: value))
+                if name != item.name {
+                    item.rename(as: name)
+                }
             }
         }
     }
--- Sources/novy/main.swift
+++ Sources/novy/main.swift
@@ -8,9 +8,15 @@ import Files
 
 let fm = FolderManager.shared
 let duplicator = Duplicator()
-let template = fm.folder(for: URL(fileURLWithPath: "/Users/sam/Projects/novy/Extras/Templates/Example"))
+let templates = fm.folder(for: "/Users/sam/Projects/novy/Extras/Templates")
+let example = templates.folder(name: "example")
 let destination = fm.folder(for: "/Users/sam/Projects/novy/Output")
+let actionstatus = fm.folder(for: "/Users/sam/Projects/ActionStatus")
 
-destination.delete()
-destination.create()
-duplicator.clone(template: template, into: destination)
+duplicator.import(project: actionstatus, into: templates)
+
+//destination.delete()
+//destination.create()
+//duplicator.clone(template: template, into: destination, substitutions: [
+//    "project": "Example"
+//])
