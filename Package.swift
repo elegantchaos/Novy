0326007a48e3d412e050675596052c26cf6f6020
Sam Deane <sam@elegantchaos.com>
2022-07-19 12:59:26 +0100

    Added tools, updated to 5.6 minimum.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.6
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 //  Created by Sam Deane on 08/06/2020.
@@ -9,20 +9,28 @@ import PackageDescription
 
 let package = Package(
     name: "novy",
+    
     platforms: [
         .macOS(.v10_13)
     ],
+    
     products: [
         .executable(name: "novy", targets: ["novy"])
     ],
+    
     dependencies: [
          .package(url: "https://github.com/elegantchaos/CommandShell.git", from: "2.1.2"),
          .package(url: "https://github.com/elegantchaos/Expressions.git", from: "1.1.1"),
          .package(url: "https://github.com/elegantchaos/Files.git", from: "1.1.3"),
          .package(url: "https://github.com/elegantchaos/Runner.git", from: "1.3.0"),
+         
+         // tools
+         .package(url: "https://github.com/elegantchaos/ActionBuilderPlugin.git", from: "1.0.7"),
+         .package(url: "https://github.com/elegantchaos/SwiftFormatterPlugin.git", from: "1.0.3"),
     ],
+    
     targets: [
-        .target(
+        .executableTarget(
             name: "novy",
             dependencies: [
                 .product(name: "CommandShell", package: "CommandShell"),
@@ -30,9 +38,12 @@ let package = Package(
                 .product(name: "Files", package: "Files"),
                 .product(name: "FilesKit", package: "Files"),
                 .product(name: "Runner", package: "Runner"),
-        ]),
+        ]
+        ),
+        
         .testTarget(
             name: "novyTests",
-            dependencies: ["novy"]),
+            dependencies: ["novy"]
+        ),
     ]
 )
